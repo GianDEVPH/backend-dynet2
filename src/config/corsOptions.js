0884b6c0d44e60ef52959eb5a254d04121ced956
GianDEVPH <70077025+GianDEVPH@users.noreply.github.com>
2025-10-28 12:02:41 +0800

    Add files via upload

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,14 +1,18 @@
-// CORS configuration options including allowed origins, methods, and headers.
+const allowedOrigins = [
+  'https://tool.dynet.nl',   // jouw frontend domein
+  'http://localhost:3000'    // voor lokaal testen
+];
 
-const allowedOrigins = require('./allowedOrigins')
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error('Not allowed by CORS'));
     }
   },
-  optionsSuccessStatus: 200,
-}
-module.exports = corsOptions
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+module.exports = corsOptions;
