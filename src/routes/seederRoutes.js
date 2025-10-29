bbddf5c88feb0f98760329131ad371db110e05fb
Oday Maleh <oday@saltserv.com>
2025-10-29 10:54:30 +0100

    Add test endpoint to seeder routes for debugging

--- src/routes/seederRoutes.js
+++ src/routes/seederRoutes.js
@@ -6,6 +6,11 @@ const { runSeeder } = require('../controllers/seederController')
 const verifyJWT = require('../middleware/verifyJWT')
 const verifyRoles = require('../middleware/verifyRoles')
 
+// Test endpoint to verify routes are working
+router.get('/test', (req, res) => {
+  res.json({ message: 'Seeder routes are working!' })
+})
+
 // Public endpoint for initial setup (REMOVE AFTER FIRST USE)
 router.post('/init/:seederType', runSeeder)
 
